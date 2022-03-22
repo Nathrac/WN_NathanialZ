c7490e4b2c93f07502ff9b214089ef183eb8f029
Nathrac <nathanial.zaina@lethbridgecollege.ca>
2022-03-21 20:43:48 -0600

    Bow And Arrow Testing

---
+++ WN22_NathanialZ_Sandbox/Assets/Scripts/1165-Design/SaveSerial.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+public class SaveSerial : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
